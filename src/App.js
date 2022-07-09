1a9afccf19bf4d5ebabcba6aa838672ec9479391
beDry2000 <106262487+beDry2000@users.noreply.github.com>
2022-07-09 13:31:46 +0700

    scroll to top use react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function useScrollTop() {
+    let location = useLocation();
+    useEffect(() => {
+        window.scrollTo({top: 0});
+    },[location])
 }
-
+function Home() {
+    useScrollTop()
+    return (
+        <>
+            <h1>Home page</h1>
+            <hr />
+            <p style={{marginTop: '150vh'}}>
+                <Link to='/contact'>Go to contact page</Link>
+            </p>
+        </>
+    )
+}
+function Contact() {
+    useScrollTop()
+    return (
+        <>
+            <h1>Contact page</h1>
+            <hr />
+            <p style={{marginTop: '150vh'}}>
+                <Link to='/'>Go to home page</Link>
+            </p>
+        </>
+    )
+}
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/contact' element={<Contact />}/>
+            </Routes>
+        </Router>
+    )
+}
 export default App;
+// function useScrollTop() {
+//     let location = useLocation();
+//     useEffect(() => {
+//         window.scrollTo({ top: 0 });
+
+//     }, [location])
+// }
+// function Home() {
+//     useScrollTop();
+//     return (
+//         <>
+//             <h1>Home Page</h1>
+//             <hr />
+//             <p style={{ marginTop: '150vh' }}>
+//                 <Link to='/contact'>Go to contact page</Link>
+//             </p>
+//         </>
+//     )
+// }
+// function Contact() {
+//     useScrollTop();
+//     return (
+//         <>
+//             <h1>Contact Page</h1>
+//             <hr />
+//             <p style={{ marginTop: '150vh' }}>
+//                 <Link to='/'>Go to home page</Link>
+//             </p>
+//         </>
+//     )
+// }
+
+
+// function App() {
+//     return (
+//         <div style={{ padding: '50px' }}>
+//             <Router>
+//                 <Routes>
+//                     <Route path="/" element={<Home />} />
+//                     <Route path="/contact" element={<Contact />} />
+//                 </Routes>
+//             </Router>
+//         </div>
+//     )
+// }
+// export default App;
